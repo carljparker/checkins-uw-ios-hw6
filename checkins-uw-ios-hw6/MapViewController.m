7dc6adb1edc1e8574a83399bb0673435bf5cea8b
Carl Parker <Carl.Parker@hbo.com>
2015-05-31 18:11:41 -0700

    enable mapview and search controller ux

--- checkins-uw-ios-hw6/MapViewController.m
+++ checkins-uw-ios-hw6/MapViewController.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (nonatomic, strong) MKLocalSearch *localSearch;
 @property (strong, nonatomic) IBOutlet MKMapView *mkMapView;
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @end
 
@@ -22,6 +23,127 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:YES];
+    
+    if (self.locationManager == nil) {
+        self.locationManager = [[CLLocationManager alloc] init];
+    }
+    
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    self.locationManager.delegate = self;
+    
+    self.mkMapView.delegate = self;
+    self.searchBar.delegate = self;
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        [self.locationManager requestWhenInUseAuthorization];
+    }
+    
+    //    [self.locationManager startUpdatingLocation];
+    self.mkMapView.showsUserLocation = YES;
+    
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    //
+    // The ...10, 10... here are the "zoom level"
+    //
+    [mapView setRegion:MKCoordinateRegionMakeWithDistance(userLocation.location.coordinate, 10, 10) animated:YES];
+    
+}
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        MKPinAnnotationView *greenPin = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"GreenPin"];
+        if (greenPin == nil) {
+            greenPin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"GreenPin" ];
+            greenPin.pinColor = MKPinAnnotationColorGreen;
+            greenPin.animatesDrop = YES;
+            greenPin.canShowCallout = YES;
+            
+            UIButton *calloutButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+            
+            [calloutButton addTarget:nil action:nil forControlEvents:UIControlEventTouchUpInside];
+            
+            greenPin.leftCalloutAccessoryView = calloutButton;
+            return  greenPin;
+        }
+        else {
+            greenPin.annotation = annotation;
+            return greenPin;
+        }
+    }
+    
+    if ([annotation isKindOfClass:[MKPlacemark class]]) {
+        MKPinAnnotationView *purplePin = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"PurplePin"];
+        if (purplePin == nil) {
+            purplePin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"GreenPin" ];
+            purplePin.pinColor = MKPinAnnotationColorPurple;
+            purplePin.animatesDrop = YES;
+            purplePin.canShowCallout = YES;
+            
+            UIButton *calloutButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+            
+            [calloutButton addTarget:nil action:nil forControlEvents:UIControlEventTouchUpInside];
+            
+            purplePin.leftCalloutAccessoryView = calloutButton;
+            return  purplePin;
+        }
+        else {
+            purplePin.annotation = annotation;
+            return purplePin;
+        }
+    }
+    
+    return nil;
+    
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    NSLog(@"%@", view.annotation.title);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    
+    NSLog(@"Location Authorization Status Changed");
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    [self.locationManager startUpdatingLocation];
+    
+    NSLog(@"Location: %@", locations);
+    
+}
+
+
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    MKLocalSearchRequest *searchRequest = [[MKLocalSearchRequest alloc] init];
+    searchRequest.region = self.mkMapView.region;
+    searchRequest.naturalLanguageQuery = searchBar.text;
+    
+    if (self.localSearch.isSearching) {
+        [self.localSearch cancel];
+        self.localSearch = nil;
+    }
+    
+    self.localSearch = [[MKLocalSearch alloc] initWithRequest:searchRequest];
+    
+    [self.localSearch startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        for (id<MKAnnotation> annotation in self.mkMapView.annotations )  {
+            [self.mkMapView removeAnnotation:annotation];
+        }
+        
+        for (MKMapItem *mapItem in response.mapItems) {
+            
+            [self.mkMapView addAnnotation:mapItem.placemark];
+            
+        }
+    }];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
