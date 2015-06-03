ab5a801b27e3af28746daa9c7f11f5c3a6647043
Carl Parker <Carl.Parker@hbo.com>
2015-06-03 08:30:46 -0700

    TC for issue using a segue from a VC embedded in a nav controller

--- checkins-uw-ios-hw6/ImageListTableViewController.m
+++ checkins-uw-ios-hw6/ImageListTableViewController.m
@@ -33,7 +33,7 @@ static NSString * customImageCellID = @"customImageCell";
     
     self.navigationItem.title = @"Checkin Gallery";
     
-    UIBarButtonItem *rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(sayHello:)];
+    UIBarButtonItem *rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(getImage:)];
     
     self.navigationItem.rightBarButtonItem = rightBarButtonItem;
     
@@ -96,9 +96,12 @@ static NSString * customImageCellID = @"customImageCell";
     
 }
 
-- (IBAction)sayHello:(id)sender
+- (IBAction)getImage:(id)sender
 {
+    
     NSLog(@"%@", @"Custom Button Clicked");
+    [self performSegueWithIdentifier:@"getImage" sender:self];
+    
 }
 
 
